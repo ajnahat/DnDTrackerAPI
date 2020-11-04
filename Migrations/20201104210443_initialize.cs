5f82bfe0774ee03563c67f0aea76f19bfaed2f0d
Anthony Nahat <anthony.nahat@yahoo.com>
2020-11-04 16:04:51 -0500

    Migration Initialize

---
+++ Migrations/20201104210443_initialize.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DnDTrackerAPI.Migrations
+{
+    public partial class initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Encounters",
+                columns: table => new
+                {
+                    EncounterId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(nullable: false),
+                    EncounterName = table.Column<string>(nullable: true),
+                    Sort = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encounters", x => x.EncounterId);
+                    table.ForeignKey(
+                        name: "FK_Encounters_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Waves",
+                columns: table => new
+                {
+                    WaveId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EncounterId = table.Column<int>(nullable: false),
+                    Sort = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Waves", x => x.WaveId);
+                    table.ForeignKey(
+                        name: "FK_Waves_Encounters_EncounterId",
+                        column: x => x.EncounterId,
+                        principalTable: "Encounters",
+                        principalColumn: "EncounterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Monsters",
+                columns: table => new
+                {
+                    WaveId = table.Column<int>(nullable: false),
+                    Index = table.Column<string>(nullable: false),
+                    Count = table.Column<int>(nullable: false),
+                    Sort = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Monsters", x => new { x.Index, x.WaveId });
+                    table.ForeignKey(
+                        name: "FK_Monsters_Waves_WaveId",
+                        column: x => x.WaveId,
+                        principalTable: "Waves",
+                        principalColumn: "WaveId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Encounters_UserId",
+                table: "Encounters",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Monsters_WaveId",
+                table: "Monsters",
+                column: "WaveId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true,
+                filter: "[UserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Waves_EncounterId",
+                table: "Waves",
+                column: "EncounterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Monsters");
+
+            migrationBuilder.DropTable(
+                name: "Waves");
+
+            migrationBuilder.DropTable(
+                name: "Encounters");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
